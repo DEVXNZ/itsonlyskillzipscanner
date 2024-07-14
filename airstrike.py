1cfd64d87fdfa35f6d29a8661594437076cc28a6
vasil <144474464+DEVXNZ@users.noreply.github.com>
2024-07-14 20:42:44 +0300

    Add files via upload

---
+++ airstrike.py
@@ -0,0 +1,21 @@
+import os
+print("*******************************************")
+print("*      SKIDO HACKS   ITSONLYSKILLZ        *")
+print("*******************************************")
+
+
+
+
+
+
+print("USE THIS TOOL IN YOUR OWN RESPONSIBILITY. EDUCATIONAL PERPOSES ONLY")
+
+
+
+
+ip=(input("IP Address: "))
+ping=os.system("ping "+str(ip))
+
+
+
+
